bb65a508f28171c9d69d963d4e3eac920ccd94bf
kimgenie0409 <kimgenie0409@gmail.com>
2023-02-17 00:39:15 +0900

    [Update] Basic Bottom Nav Controller

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,54 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:instagram_clone/src/components/image_data.dart';
+import 'package:instagram_clone/src/controller/bottom_nav_controller.dart';
 
-class App extends StatelessWidget {
+class App extends GetView<BottomNavController> {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
-      child: Scaffold(
-        appBar: AppBar(),
-        body: Container(),
-        bottomNavigationBar: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          showSelectedLabels: false,
-          showUnselectedLabels: false,
-          currentIndex: 0,
-          elevation: 0,
-          onTap: (value) {},
-          items: [
-            BottomNavigationBarItem(
-              icon: ImageData(IconsPaths.homeOff),
-              activeIcon: ImageData(IconsPaths.homeOn),
-              label: 'home',
-            ),
-            BottomNavigationBarItem(
-              icon: ImageData(IconsPaths.searchOff),
-              activeIcon: ImageData(IconsPaths.searchOn),
-              label: 'home',
-            ),
-            BottomNavigationBarItem(
-              icon: ImageData(IconsPaths.uploadIcon),
-              label: 'home',
-            ),
-            BottomNavigationBarItem(
-              icon: ImageData(IconsPaths.activeOff),
-              activeIcon: ImageData(IconsPaths.activeOn),
-              label: 'home',
-            ),
-            BottomNavigationBarItem(
-              icon: Container(
-                width: 30,
-                height: 30,
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Colors.grey,
-                ),
+      child: Obx(
+        () => Scaffold(
+          appBar: AppBar(),
+          body: Container(),
+          bottomNavigationBar: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            currentIndex: controller.pageIndex.value,
+            elevation: 0,
+            onTap: controller.changeBottomNav,
+            items: [
+              BottomNavigationBarItem(
+                icon: ImageData(IconsPaths.homeOff),
+                activeIcon: ImageData(IconsPaths.homeOn),
+                label: 'home',
               ),
-              label: 'home',
-            ),
-          ],
+              BottomNavigationBarItem(
+                icon: ImageData(IconsPaths.searchOff),
+                activeIcon: ImageData(IconsPaths.searchOn),
+                label: 'home',
+              ),
+              BottomNavigationBarItem(
+                icon: ImageData(IconsPaths.uploadIcon),
+                label: 'home',
+              ),
+              BottomNavigationBarItem(
+                icon: ImageData(IconsPaths.activeOff),
+                activeIcon: ImageData(IconsPaths.activeOn),
+                label: 'home',
+              ),
+              BottomNavigationBarItem(
+                icon: Container(
+                  width: 30,
+                  height: 30,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.grey,
+                  ),
+                ),
+                label: 'home',
+              ),
+            ],
+          ),
         ),
       ),
       onWillPop: () async {
