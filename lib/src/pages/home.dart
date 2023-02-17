0065659d563a4cec6904525b8d39d3cf9cb66ffc
kimgenie0409 <kimgenie0409@gmail.com>
2023-02-18 00:27:15 +0900

    [Update] Complete Story Area UI

--- lib/src/pages/home.dart
+++ lib/src/pages/home.dart
@@ -5,11 +5,50 @@ import 'package:instagram_clone/src/components/image_data.dart';
 class Home extends StatelessWidget {
   const Home({super.key});
 
+  Widget _myStory() {
+    return Stack(
+      children: [
+        AvatarWidget(
+          type: AvatarType.TYPE2,
+          thumbPath:
+              'https://blog.kakaocdn.net/dn/5UYz8/btq4diRXkGE/HkHufR4G8X4bIX3h3lNjck/img.jpg',
+          size: 70,
+        ),
+        Positioned(
+          right: 5,
+          bottom: 0,
+          child: Container(
+            width: 25,
+            height: 25,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.blue,
+              border: Border.all(color: Colors.white, width: 2),
+            ),
+            child: const Center(
+              child: Text(
+                '+',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  height: 1.1,
+                ),
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
   Widget _storyBoardList() {
     return SingleChildScrollView(
       scrollDirection: Axis.horizontal,
-      child: Row(
-        children: List.generate(
+      child: Row(children: [
+        const SizedBox(width: 20),
+        _myStory(),
+        const SizedBox(width: 5),
+        ...List.generate(
           100,
           (index) => AvatarWidget(
             type: AvatarType.TYPE1,
@@ -17,7 +56,7 @@ class Home extends StatelessWidget {
                 'https://t1.kakaocdn.net/thumb/R1920x0.fwebp.q100/?fname=https%3A%2F%2Ft1.kakaocdn.net%2Fkakaocorp%2Fkakaocorp%2Fadmin%2Fservice%2Fa85d0594017900001.jpg',
           ),
         ),
-      ),
+      ]),
     );
   }
 
