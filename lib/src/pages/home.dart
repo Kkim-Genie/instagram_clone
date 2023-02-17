ae830aa4f4046825082f41ce14d8bd1afa74b4dc
kimgenie0409 <kimgenie0409@gmail.com>
2023-02-18 00:09:20 +0900

    [Update] AppBar and Basic Story Area

---
+++ lib/src/pages/home.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/src/components/avatar_widget.dart';
+import 'package:instagram_clone/src/components/image_data.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  Widget _storyBoardList() {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: List.generate(
+          100,
+          (index) => AvatarWidget(
+            type: AvatarType.TYPE1,
+            thumbPath:
+                'https://t1.kakaocdn.net/thumb/R1920x0.fwebp.q100/?fname=https%3A%2F%2Ft1.kakaocdn.net%2Fkakaocorp%2Fkakaocorp%2Fadmin%2Fservice%2Fa85d0594017900001.jpg',
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: ImageData(IconsPaths.logo, width: 270),
+        actions: [
+          GestureDetector(
+            onTap: () {},
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: ImageData(
+                IconsPaths.directMessage,
+                width: 50,
+              ),
+            ),
+          )
+        ],
+      ),
+      body: ListView(
+        children: [
+          _storyBoardList(),
+          // _postList(),
+        ],
+      ),
+    );
+  }
+}
